8188aae40147e0e166c3b7b7b97fe16b64c39436
Cheong DongMin <aehdals9900@gmail.com>
2020-11-04 20:09:47 +0900

    11_04 잉기모링

--- src/main/java/com/fp/neezit/product/controller/ProductController.java
+++ src/main/java/com/fp/neezit/product/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.fp.neezit.product.model.service.ProductService;
+import com.fp.neezit.product.model.vo.Product;
 import com.fp.neezit.product.model.vo.ProductCategory;
 import com.fp.neezit.user.model.vo.User;
 import com.fp.neezit.user.model.vo.UserMaster;
@@ -51,17 +52,36 @@ public class ProductController {
    @RequestMapping(value = "productInsertPage.do" , method = RequestMethod.GET)
    public String getGoodsRegister(Model model,HttpSession session) throws Exception{
 	   
-//	      User u = (User)session.getAttribute("loginUser");
-//	      
-//	      UserMaster master = pService.getMaster(u);
+	      User u = (User)session.getAttribute("loginUser");
 	      
+	      UserMaster master = pService.getMaster(u);
+	      
+	      System.out.println(master);
 	      // 상품 카테고리 3분류
 	      List<ProductCategory> category = null;
 	      category = pService.category();
 	      model.addAttribute("category", JSONArray.fromObject(category));
 	      
+	      model.addAttribute("master",master);
+	      
 	      return "user/product/productInsert";
    }
-
    
+   @RequestMapping(value = "pInsert.do")
+   public String insertProduct(Model model,Product product) {
+	   
+	   System.out.println(product);
+	   
+		
+		  int result = pService.insertProduct(product);
+		 
+		  if(result==1) {
+			  System.out.println("삽입 완료");
+		  }else {
+			  System.out.println("삽입 실패");
+		  }
+		 
+	   return "user/product/productDetail";
+   }
+
 }
