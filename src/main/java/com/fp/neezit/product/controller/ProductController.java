cf9504255b1aebca33d6fb361f80cae5ee8993bc
Cheong DongMin <aehdals9900@gmail.com>
2020-11-04 16:01:15 +0900

    11_04 한거

--- src/main/java/com/fp/neezit/product/controller/ProductController.java
+++ src/main/java/com/fp/neezit/product/controller/ProductController.java
@@ -54,9 +54,9 @@ public class ProductController {
    @RequestMapping(value = "productInsert.do" , method = RequestMethod.GET)
    public String getGoodsRegister(Model model,HttpSession session) throws Exception{
 	   
-	      User u = (User)session.getAttribute("loginUser");
-	      
-	      UserMaster master = pService.getMaster(u);
+//	      User u = (User)session.getAttribute("loginUser");
+//	      
+//	      UserMaster master = pService.getMaster(u);
 	      
 	      // 상품 카테고리 3분류
 	      List<ProductCategory> category = null;
